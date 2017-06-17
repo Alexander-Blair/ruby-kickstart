b7b7ff05663da8cba7b5c9b6bf4ff961408b05be
Alexander-Blair <zanderb@hotmail.co.uk>
2017-06-17 19:11:53 +0100

    Exercise 7 done

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,13 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  arr_string = ""
+  (string.size - 1).times do |i|
+    arr_string << string[i + 1] if string[i].downcase == 'r'
+  end
+  arr_string
 end
